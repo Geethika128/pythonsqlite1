584deca0c7c0d1f9741c1d837e5ebdd548ff4725
Geethika128 <143965851+Geethika128@users.noreply.github.com>
2024-09-14 10:58:08 -0400

    SQL3.py

--- SQL3.py
+++ SQL3.py
@@ -3,8 +3,11 @@ import sqlite3
 connection = sqlite3.connect('example.db')
 # Create a cursor object to interact with the database
 cursor = connection.cursor()
-cursor.execute('''CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade REAL)''')
-cursor.execute("INSERT INTO students (name, grade) VALUES ('Alice', 85.5)")
+cursor.execute('''CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT,  subject TEXT , age INT ,grade REAL)''')
+cursor.execute("INSERT INTO students (id,name,subject,age grade) VALUES (101,'Alice','computers',22,85.5)")
+cursor.execute("INSERT INTO students (id,name,subject,age grade) VALUES (102,'Geethika','Da',25,92.6)")
+cursor.execute("INSERT INTO students (id,name,subject,age grade) VALUES (103,'Chinnu','DMT',26, 75.9)")
+cursor.execute("INSERT INTO students (id,name,subject,age grade) VALUES (104,'sai','PDM',24, 88.3)")
 cursor.execute("SELECT * FROM students")
 rows = cursor.fetchall()
 for row in rows:
@@ -15,6 +18,6 @@ try:
    cursor.execute("SELECT * FROM non_existing_table")
 except sqlite3.OperationalError as e:
   print(f"An error occurred: {e}")
-cursor.execute("INSERT INTO students (name, grade) VALUES (?, ?)", ('Bob', 92.3))
+cursor.execute("INSERT INTO students (id,name,subject,age, grade) VALUES (?, ?,?,?)", (110,'Bob','ADSD',27, 92.3))
 #closing connection
 connection.close()
